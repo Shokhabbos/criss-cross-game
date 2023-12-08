cdee7ce5899ddf9649f059c7a6f0789c44d3c108
shokhabbos <urinovs999@gmail.com>
2023-12-08 20:58:22 +0500

    updated: control flow convert to looping methods

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ class Board extends Component {
   render() {
     return (
       <div className="board">
-        {this.props.board.map((cell, i) => this.renderCell(i))}
+        {this.props.board.map((_, i) => this.renderCell(i))}
       </div>
     );
   }
@@ -42,31 +42,32 @@ class App extends Component {
     };
   }
 
-  handleClick(i) {
-    const board = this.state.board.slice();
-    const winner = this.state.winner;
+  handleClick = (i) => {
+    const { board, winner, gameStarted, currentPlayer } = this.state;
 
     if (winner || board[i]) {
       return;
     }
-    if (!this.state.gameStarted) {
+    if (!gameStarted) {
       this.setState({ gameStarted: true });
     }
 
-    board[i] = this.state.currentPlayer;
-    this.setState({ board: board }, () => {
+    const newBoard = [...board];
+    newBoard[i] = currentPlayer;
+
+    this.setState({ board: newBoard }, () => {
       this.checkWinner();
       this.togglePlayer();
     });
-  }
+  };
 
-  togglePlayer() {
+  togglePlayer = () => {
     this.setState({
       currentPlayer: this.state.currentPlayer === "X" ? "O" : "X",
     });
-  }
+  };
 
-  checkWinner() {
+  checkWinner = () => {
     const lines = [
       [0, 1, 2],
       [3, 4, 5],
@@ -78,73 +79,72 @@ class App extends Component {
       [2, 4, 6],
     ];
 
-    for (let line of lines) {
-      const [a, b, c] = line;
-      const board = this.state.board;
+    const { board } = this.state;
 
-      if (board[a] && board[a] === board[b] && board[a] === board[c]) {
-        this.setState({ winner: board[a] });
-        return;
-      }
+    const winnerLine = lines.find(
+      ([a, b, c]) => board[a] && board[a] === board[b] && board[a] === board[c]
+    );
+
+    if (winnerLine) {
+      const [a] = winnerLine;
+      this.setState({ winner: board[a] });
+      return;
     }
 
-    if (this.state.board.every((cell) => cell)) {
+    if (board.every(Boolean)) {
       this.setState({ winner: "draw" });
     }
-  }
+  };
 
-  resetGame() {
+  resetGame = () => {
     this.setState({
       board: Array(9).fill(null),
       currentPlayer: "X",
       winner: null,
     });
-  }
+  };
 
-  help() {
-    const board = this.state.board.slice();
-    const emptyCells = [];
-
-    for (let i = 0; i < board.length; i++) {
-      if (!board[i]) {
-        emptyCells.push(i);
-      }
-    }
+  help = () => {
+    const { board, currentPlayer } = this.state;
+    const emptyCells = board
+      .map((cell, i) => (!cell ? i : null))
+      .filter((el) => el !== null);
 
     if (emptyCells.length > 0) {
       const randomCell =
         emptyCells[Math.floor(Math.random() * emptyCells.length)];
-      board[randomCell] = this.state.currentPlayer;
-      this.setState({ board: board }, () => {
+      const newBoard = board.map((cell, i) =>
+        i === randomCell ? currentPlayer : cell
+      );
+
+      this.setState({ board: newBoard }, () => {
         this.checkWinner();
         this.togglePlayer();
       });
     } else {
       this.setState({ winner: "draw" });
     }
-  }
+  };
+
   render() {
-    const currentPlayer = this.state.currentPlayer;
-    const winner = this.state.winner;
+    const { currentPlayer, winner, gameStarted } = this.state;
     const status = winner
       ? `Player: ${winner} wins!`
       : `Player:${currentPlayer === "X" ? "X" : "O"}`;
-    const isGameFinished =
-      winner !== null || this.state.board.every((cell) => cell);
+    const isGameFinished = winner !== null || this.state.board.every(Boolean);
 
     return (
       <div className="App">
         <header className="App-header">
-          <h1>
-            <a href="https://github.com/Shokhabbos/criss-cross-game">
+          <h1 className="header-title">
+            <a
+              className="link"
+              href="https://github.com/Shokhabbos/criss-cross-game"
+            >
               Criss-Cross Game
             </a>
           </h1>
-          <h5
-            className={`game-start-message ${
-              this.state.gameStarted ? "hidden" : ""
-            }`}
-          >
+          <h5 className={`game-start-message ${gameStarted ? "hidden" : ""}`}>
             You can press any buttons to start the game
           </h5>
           <h2>{status}</h2>
@@ -154,11 +154,11 @@ class App extends Component {
             board={this.state.board}
             onClick={(i) => this.handleClick(i)}
           />
-          <button onClick={() => this.resetGame()} className="action-button">
+          <button onClick={this.resetGame} className="action-button">
             New Game
           </button>
           <button
-            onClick={() => this.help()}
+            onClick={this.help}
             className={`action-button ${isGameFinished ? "disabled" : ""}`}
             disabled={isGameFinished}
           >
